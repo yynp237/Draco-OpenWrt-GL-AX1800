fb74a3d65c2ea27a3d730a1a714a610e12528756
Draco <draco.coder@gmail.com>
2022-11-16 11:16:37 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -53,78 +53,78 @@ module.exports = [
     name: 'luci-app-xlnetacc',
     desc: '迅雷快鸟',
   },
-  {
-    name: 'luci-app-appfilter',
-    desc: '应用过滤'
-  },
+//   {
+//     name: 'luci-app-appfilter',
+//     desc: '应用过滤'
+//   },
   {
     name: 'luci-app-cpufreq',
     desc: 'CPU 性能优化调节',
   },
-  // {
-  //   name: 'luci-app-samba4',
-  //   desc: 'samba4文件共享',
-  // },
-  // {
-  //   name: 'luci-app-sqm',
-  //   desc: '流量智能队列管理(QOS)',
-  // },
-  // {
-  //   name: 'luci-app-advancedsetting',
-  //   desc: '系统高级设置',
-  // },
+//   {
+//     name: 'luci-app-samba4',
+//     desc: 'samba4文件共享',
+//   },
+//   {
+//     name: 'luci-app-sqm',
+//     desc: '流量智能队列管理(QOS)',
+//   },
+//   {
+//     name: 'luci-app-advancedsetting',
+//     desc: '系统高级设置',
+//   },
   {
     name: 'luci-app-ttyd',
     desc: 'ttyd 终端',
   },
-  // {
-  //   name: 'luci-app-unblockneteasemusic',
-  //   desc: '解锁网易云音乐 (无界面)',
-  // },
-  // {
-  //   name: 'luci-app-syncdial',
-  //   desc: '多拨虚拟网卡（需要光猫支持多拨）',
-  // },
-  {
-    name: 'luci-app-passwall',
-    desc: 'passwall代理软件',
-  },
-  {
-    name: 'luci-app-ssr-plus',
-    desc: 'ssr-plus 代理软件',
-  },
-  // {
-  //   name: 'luci-app-aliddns',
-  //   desc: '阿里云ddns插件',
-  // },
-  // {
-  //   name: 'luci-app-ddns',
-  //   desc: '动态域名 DDNS',
-  // },
-  // {
-  //   name: 'luci-app-eqos',
-  //   desc: '依IP地址限速',
-  // },
-  // {
-  //   name: 'luci-app-filetransfer',
-  //   desc: '文件传输',
-  // },
-  // {
-  //   name: 'luci-app-ddnsto',
-  //   desc: 'DDNSTO 内网穿透',
-  // },
-  // {
-  //   name: 'luci-app-mwan3',
-  //   desc: 'MWAN3负载均衡',
-  // },
-  // {
-  //   name: 'luci-app-mwan3helper',
-  //   desc: ' MWAN3分流助手',
-  // },
-  // {
-  //   name: 'luci-app-wrtbwmon',
-  //   desc: '实时流量监测',
-  // },
+//   {
+//     name: 'luci-app-unblockneteasemusic',
+//     desc: '解锁网易云音乐 (无界面)',
+//   },
+//   {
+//     name: 'luci-app-syncdial',
+//     desc: '多拨虚拟网卡（需要光猫支持多拨）',
+//   },
+//   {
+//     name: 'luci-app-passwall',
+//     desc: 'passwall代理软件',
+//   },
+//   {
+//     name: 'luci-app-ssr-plus',
+//     desc: 'ssr-plus 代理软件',
+//   },
+//   {
+//     name: 'luci-app-aliddns',
+//     desc: '阿里云ddns插件',
+//   },
+//   {
+//     name: 'luci-app-ddns',
+//     desc: '动态域名 DDNS',
+//   },
+//   {
+//     name: 'luci-app-eqos',
+//     desc: '依IP地址限速',
+//   },
+//   {
+//     name: 'luci-app-filetransfer',
+//     desc: '文件传输',
+//   },
+//   {
+//     name: 'luci-app-ddnsto',
+//     desc: 'DDNSTO 内网穿透',
+//   },
+//   {
+//     name: 'luci-app-mwan3',
+//     desc: 'MWAN3负载均衡',
+//   },
+//   {
+//     name: 'luci-app-mwan3helper',
+//     desc: ' MWAN3分流助手',
+//   },
+//   {
+//     name: 'luci-app-wrtbwmon',
+//     desc: '实时流量监测',
+//   },
   {
     name: 'luci-app-serverchan',
     desc: '微信/Telegram推送',
@@ -133,16 +133,16 @@ module.exports = [
     name: 'luci-app-transmission',
     desc: 'BT下载工具',
   },
-  // {
-  //   name: 'luci-app-jd-dailybonus',
-  //   desc: '京东签到服务',
-  // },
-  // {
-  //   name: 'luci-app-frpc',
-  //   desc: 'frpc 网络代理',
-  // },
-  // {
-  //   name: 'luci-app-frps',
-  //   desc: 'frps 网络代理',
-  // },
+//   {
+//     name: 'luci-app-jd-dailybonus',
+//     desc: '京东签到服务',
+//   },
+//   {
+//     name: 'luci-app-frpc',
+//     desc: 'frpc 网络代理',
+//   },
+//   {
+//     name: 'luci-app-frps',
+//     desc: 'frps 网络代理',
+//   },
 ];
