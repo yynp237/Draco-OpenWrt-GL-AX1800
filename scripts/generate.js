f71a2ec6bee2cf916f255ef1ee7c318c2f4ad3e7
Draco <draco.coder@gmail.com>
2023-03-27 13:54:19 +0800

    feat: 更新模板设备

--- scripts/generate.js
+++ scripts/generate.js
@@ -25,6 +25,8 @@ const GenerateFeedsConfig = (name, uri, branch) => {
 const UIDevices = [
   'target_wlan_ap-gl-ax1800',
   'target_wlan_ap-gl-axt1800',
+  // 'target_wlan_ap-gl-ax1800-5-4',
+  // 'target_wlan_ap-gl-axt1800-5-4',
   // 'target_ipq40xx_gl-a1300',
   // 'target_mt7981_gl-mt2500',
   // 'target_mt7981_gl-mt3000',
